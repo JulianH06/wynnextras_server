7f3a0c15e789e67fe0e9931b6a299569074dc3dd
JulianH06 <Julian.hirth@gmail.com>
2026-01-25 20:08:29 +0100

    a

--- src/main/java/com/julianh06/wynnextras_server/WynnextrasServerApplication.java
+++ src/main/java/com/julianh06/wynnextras_server/WynnextrasServerApplication.java
@@ -3,6 +3,7 @@ package com.julianh06.wynnextras_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -13,6 +14,7 @@ public class WynnextrasServerApplication {
 	}
 
 	@GetMapping
+	@ResponseBody
 	public String helloWorld() {
 		return "Fick dich Muecke!";
 	}
